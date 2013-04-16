effeacc772035abd9ac708fcc337ebe0fea6365b
JayceTDE <jaycemp@gmail.com>
2013-04-16 11:26:38 -0600

    Removed event emitter dependency

--- explorer.js
+++ explorer.js
@@ -1,68 +1,25 @@
 'use strict';
 
-/* Depends on
- *  jQuery
- *  EventEmitter
- *  browser (document)
- */
-
-function inherits (child, parent) {
-	for (var key in parent.prototype) {
-		if (parent.prototype.hasOwnProperty(key) {
-			child.prototype[key] = parent.prototype[key];
-		}
-	}
-}
-
-var Root = function (path, options) {
-
-	if (!(this instanceof Root)) {
-		return new Root(path, options);
-	}
-
-	this._path = path;
-	this._options = options;
-
-};
-
-inherits(Root, EventEmitter);
-
-Root.prototype.get = function (path, callback) {
-	
-	var self = this;
-
-	self.emit('loading', path);
-
-	$.get(self._path + path, function (data) {
-
-		self.emit('loaded', path, data);
-
-		callback(data);
-
-	});
-
-};
-
 var Tree = function (name, root) {
 
 	if (!(this instanceof Tree)) {
 		return new Tree(name, root);
 	}
 
+	this.jq = $(this);
+
 	this.name = name;
 	this._root = root;
 
 	this.current = null;
 
-	this.on('loading', this.loading);
-	this.on('loaded', this.loaded);
+	this.jq.bind('loading', this.loading);
+	this.jq.bind('loaded', this.loaded);
 
 	// initialize this.$el
 	this.$el = $(document.createElement('div'));
 
 };
-
-inherits(Tree, EventEmitter);
 
 Tree.prototype.$ = function (selector) {
 	return this.$el.find(selector);
@@ -72,11 +29,11 @@ Tree.prototype.get = function (path, callback) {
 
 	var self = this;
 
-	self.emit('loading', path);
+	self.jq.trigger('loading', path);
 
 	$.get(self._root + path, function (data) {
 
-		self.emit('loaded', path, data);
+		self.jq.trigger('loaded', [ path, data ]);
 
 		callback(data);
 
@@ -159,13 +116,13 @@ Tree.prototype.refresh = function () {
 
 };
 
-Tree.prototype.loading = function (path) {
+Tree.prototype.loading = function (event, path) {
 
 	this.$('[data-path=' + path + '] > .tree-item').addClass('tree-loading');
 
 };
 
-Tree.prototype.loaded = function (path) {
+Tree.prototype.loaded = function (event, path) {
 
 	this.$('[data-path=' + path + '] > .tree-item').removeClass('tree-loading');
 
